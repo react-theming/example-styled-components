12acb33daa6169ce039789a5a4917cafd285db70
Alexsandr Litkevich <alexshow2362@gmail.com>
2022-02-07 11:20:25 +0400

    clear preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,10 +6,6 @@ import {
 import { oneTheme } from "../src/Components/Ui/Themes/themeOne";
 import { twoTheme } from "../src/Components/Ui/Themes/themeTwo";
 
-
-
-
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -19,39 +15,5 @@ export const parameters = {
     },
   },
 }
-
-export const globalTypes = {
-  theme: {
-    name: 'Theme',
-    description: 'Global theme for components',
-    defaultValue: 'Default',
-    toolbar: {
-      icon: 'switchalt',
-      items: ['oneTheme', 'twoTheme'],
-      showName: true,
-    },
-  },
-};
-
-const getTheme = (themeName) => {
-  switch (themeName) {
-    case 'oneTheme':
-      return oneTheme;
-    default:
-      return twoTheme;
-  }
-};
-
-
-const WithTest = (Story, context) => {
-  const theme = getTheme(context.globals.theme);
-  return (
-      <ThemeProvider theme={theme}>
-        <Story {...context}/>
-      </ThemeProvider>
-  );
-};
-
-// export const  decorators = [WithTest]
 
 export const decorators = [withThemes(ThemeProvider, [twoTheme, oneTheme])];
--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { Button }  from './Components/Ui/Button'
 
 const App = () => {
     const [toggleTheme, setToggleTheme ] = useState(false);
-
     return (
         <Button onClick={() => setToggleTheme(!toggleTheme)}>Toggle theme</Button>
     );
